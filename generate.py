9a6a7e3ec4e73eac08e9317cbc6dfbee81623db2
Chris Alexiuk <c.s.alexiuk@gmail.com>
2023-03-23 01:03:01 -0400

    Added Dockerfile for inference

--- generate.py
+++ generate.py
@@ -144,7 +144,7 @@ gr.Interface(
     ],
     title="🦙🌲 Alpaca-LoRA",
     description="Alpaca-LoRA is a 7B-parameter LLaMA model finetuned to follow instructions. It is trained on the [Stanford Alpaca](https://github.com/tatsu-lab/stanford_alpaca) dataset and makes use of the Huggingface LLaMA implementation. For more information, please visit [the project's website](https://github.com/tloen/alpaca-lora).",
-).launch()
+).launch(server_name="0.0.0.0")
 
 # Old testing code follows.
 
