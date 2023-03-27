00990cd06744dd17ab172ac70b665e3d539510a6
Eric J. Wang <eric.james.wang@gmail.com>
2023-03-27 13:46:33 -0400

    fix TrainingArgs

--- finetune.py
+++ finetune.py
@@ -231,7 +231,7 @@ def train(
             ddp_find_unused_parameters=False if ddp else None,
             group_by_length=group_by_length,
             report_to="wandb" if use_wandb else None,
-            wandb_run_name=wandb_run_name if use_wandb else None
+            run_name=wandb_run_name if use_wandb else None
         ),
         data_collator=transformers.DataCollatorForSeq2Seq(
             tokenizer, pad_to_multiple_of=8, return_tensors="pt", padding=True
