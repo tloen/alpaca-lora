d597ad800a0a1e91d389fcefc5366a943a839541
Eric Wang <eric.james.wang@gmail.com>
2023-03-28 09:41:49 -0700

    gradio_share -> share_gradio

--- generate.py
+++ generate.py
@@ -27,7 +27,7 @@ def main(
     load_8bit: bool = False,
     base_model: str = "",
     lora_weights: str = "tloen/alpaca-lora-7b",
-    gradio_share: bool = False,
+    share_gradio: bool = False,
 ):
     assert (
         base_model
@@ -145,7 +145,7 @@ def main(
         ],
         title="🦙🌲 Alpaca-LoRA",
         description="Alpaca-LoRA is a 7B-parameter LLaMA model finetuned to follow instructions. It is trained on the [Stanford Alpaca](https://github.com/tatsu-lab/stanford_alpaca) dataset and makes use of the Huggingface LLaMA implementation. For more information, please visit [the project's website](https://github.com/tloen/alpaca-lora).",  # noqa: E501
-    ).launch(share=gradio_share)
+    ).launch(share=share_gradio)
     # Old testing code follows.
 
     """
