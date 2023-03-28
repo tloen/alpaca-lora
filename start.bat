9ede6851b18fd170be14edf5b6e1670a31251af4
Paillat <git@electronique.cc>
2023-03-28 08:39:10 +0200

    Renamed api file

--- start.bat
+++ start.bat
@@ -1,5 +1,5 @@
 @echo off
-start cmd /k "conda activate finetune && python api.py"
+start cmd /k "conda activate finetune && python generate.py"
 start cmd /k "conda activate finetune && cd bot && python main.py"
 @echo on
 echo Started the bot and the generator. Close the windows to stop the bot.
