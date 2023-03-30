4367a43fcb8c47ff1e520777ad7e60b622edea4d
Chris Alexiuk <114439245+chris-alexiuk@users.noreply.github.com>
2023-03-29 20:04:13 -0400

    Added Dockerfile and docker-compose.yml (#207)
    * Added Dockerfile for inference
    
    * Added instructions for Dockerfile
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Pass env through Dockerfile
    
    * Added docker compose setup and instructions
    
    * Added more environment options
    
    * Set a safer default mount point
    
    * add docker-compose changes
    
    * Added Dockerfile for inference
    
    * Added instructions for Dockerfile
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Pass env through Dockerfile
    
    * Added docker compose setup and instructions
    
    * Added more environment options
    
    * Set a safer default mount point
    
    * add to gitignore, update to new generate.py
    
    * add docker ignore, simplify docker compose file
    
    * add back missing requirements
    
    * Adjustments to compose and generate.py, added Docker to README.md
    
    * Linting adjust to Black
    
    * Adjusting import linting
    
    * Update README.md
    
    * Update README.md
    
    * Removed comment by original Dockerfile creator.
    
    Comment not necessary.
    
    * cleanup README
    
    Co-authored-by: Francesco Saverio Zuppichini <zuppif@usi.ch>
    
    ---------
    
    Co-authored-by: Francesco Saverio Zuppichini <zuppif@usi.ch>
    Co-authored-by: Chris Alexiuk <c.s.alexiuk@gmail.com>
    Co-authored-by: ElRoberto538 <>
    Co-authored-by: Sam Sipe <samsipe@gmail.com>
    Co-authored-by: Eric J. Wang <eric.james.wang@gmail.com>

--- generate.py
+++ generate.py
@@ -1,3 +1,4 @@
+import os
 import sys
 
 import fire
@@ -29,6 +30,7 @@ def main(
     server_name: str = "127.0.0.1",  # Allows to listen on all interfaces by providing '0.0.0.0'
     share_gradio: bool = False,
 ):
+    base_model = base_model or os.environ.get("BASE_MODEL", "")
     assert (
         base_model
     ), "Please specify a --base_model, e.g. --base_model='decapoda-research/llama-7b-hf'"
@@ -146,7 +148,7 @@ def main(
         ],
         title="🦙🌲 Alpaca-LoRA",
         description="Alpaca-LoRA is a 7B-parameter LLaMA model finetuned to follow instructions. It is trained on the [Stanford Alpaca](https://github.com/tatsu-lab/stanford_alpaca) dataset and makes use of the Huggingface LLaMA implementation. For more information, please visit [the project's website](https://github.com/tloen/alpaca-lora).",  # noqa: E501
-    ).launch(server_name=server_name, share=share_gradio)
+    ).launch(server_name="0.0.0.0", share=share_gradio)
     # Old testing code follows.
 
     """
