d05cc85416d6e9966be0f8caa366e32a1d14ad9c
yamashi <yamashi@users.noreply.github.com>
2023-04-06 13:14:32 +0200

    Update prompter.py

--- utils/prompter.py
+++ utils/prompter.py
@@ -15,7 +15,7 @@ class Prompter(object):
         if not template_name:
             # Enforce the default here, so the constructor can be called with '' and will not break.
             template_name = "alpaca"
-        file_name = osp.join(base_path, "templates", f"{template_name}.json")
+        file_name = osp.join(base_path, f"{template_name}.json")
         if not osp.exists(file_name):
             raise ValueError(f"Can't read {file_name}")
         with open(file_name) as fp:
