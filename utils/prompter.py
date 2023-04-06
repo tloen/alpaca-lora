ba9abadf0945ae1f0f6265300430ef816884d3b2
yamashi <yamashi@users.noreply.github.com>
2023-04-06 13:04:30 +0200

    Update prompter.py

--- utils/prompter.py
+++ utils/prompter.py
@@ -10,12 +10,12 @@ from typing import Union
 class Prompter(object):
     __slots__ = ("template", "_verbose")
 
-    def __init__(self, template_name: str = "", verbose: bool = False):
+    def __init__(self, template_name: str = "" base_path: str = "", verbose: bool = False):
         self._verbose = verbose
         if not template_name:
             # Enforce the default here, so the constructor can be called with '' and will not break.
             template_name = "alpaca"
-        file_name = osp.join("templates", f"{template_name}.json")
+        file_name = osp.join(base_path, "templates", f"{template_name}.json")
         if not osp.exists(file_name):
             raise ValueError(f"Can't read {file_name}")
         with open(file_name) as fp:
