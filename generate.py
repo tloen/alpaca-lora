c1e38dec743c2481e6e5b7e77509ea4fbab73919
Pascal Andermatt <p.andermatt@me.com>
2023-04-26 13:26:38 +0200

    Fix generate.py
    It looks like `generate_params` are duplicated, so i used the variable

--- generate.py
+++ generate.py
@@ -147,13 +147,7 @@ def main(
 
         # Without streaming
         with torch.no_grad():
-            generation_output = model.generate(
-                input_ids=input_ids,
-                generation_config=generation_config,
-                return_dict_in_generate=True,
-                output_scores=True,
-                max_new_tokens=max_new_tokens,
-            )
+            generation_output = model.generate(**generate_params)
         s = generation_output.sequences[0]
         output = tokenizer.decode(s)
         yield prompter.get_response(output)
